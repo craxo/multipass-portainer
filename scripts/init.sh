f3828de2c8598e00e56c8d96015cbb81138dd771
Fredrik <fredrik.hove@ams.no>
2021-12-21 11:17:34 +0100

    Install docker according to docker documentation

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,21 @@
+
+echo "Get updates and install ca-certificates, curl, gnupg and lsb-release"
+sudo apt-get update
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 echo "Installing Docker"
-sudo snap install docker
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 echo "Create docker group"
 sudo groupadd docker
