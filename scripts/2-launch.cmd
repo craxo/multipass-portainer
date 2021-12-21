d99d2fc59a502f7e72902d6169f05f89bb899e49
Fredrik <fredrik.hove@ams.no>
2021-12-21 09:31:31 +0100

    Added apostrophes to the network name switch

--- scripts/2-launch.cmd
+++ scripts/2-launch.cmd
@@ -3,5 +3,5 @@ ECHO Launching Multipass VM.
 multipass networks
 echo Enter name of network to bridge:
 set /p variable=
-multipass launch -v --name portainer --network name=%variable%,mode=manual
+multipass launch -v --name portainer --network name="%variable%",mode=manual
 EXIT
