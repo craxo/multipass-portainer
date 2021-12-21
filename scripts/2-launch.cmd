f71dce7971471dd699bfeea953a60f544dd12930
Fredrik <fredrik.hove@ams.no>
2021-12-21 11:16:48 +0100

    Removed excessive mode in launch command

--- scripts/2-launch.cmd
+++ scripts/2-launch.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 ECHO Launching Multipass VM.
 multipass networks
-echo Enter name of network to bridge (ie. Ethernet):
+echo Enter full name of network to bridge (ie. 'Ethernet 4'):
 set /p variable=
-multipass launch -v --name portainer --network name="%variable%",mode=manual
+multipass launch -v --name portainer --network name="%variable%"
 EXIT
